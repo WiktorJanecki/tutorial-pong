f9250ecef8d4374cc31c7e4b107b2ad92dad8384
Wiktor Janecki <j.wiktor05@interia.pl>
2024-12-04 20:18:08 +0100

    Add gameover

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use miniquad::window::quit;
 
 #[macroquad::main("Tytuł Aplikacji")]
 async fn main() {
@@ -9,35 +10,40 @@ async fn main() {
     let paddle_width = 20.;
     let paddle_height = 200.;
     let paddle_speed = 500.;
-    let mut first_paddle_pos = Vec2::new(0.,0.);
-    let mut second_paddle_pos = Vec2::new(screen_width()-paddle_width,0.);
+    let mut paddle_pos = Vec2::new(0.,0.);
 
     loop{
         let delta_time = get_frame_time();
 
         if is_key_down(KeyCode::Up){
-            first_paddle_pos.y  -= paddle_speed * delta_time;
-            second_paddle_pos.y -= paddle_speed * delta_time;
+            paddle_pos.y  -= paddle_speed * delta_time;
             
         }
         if is_key_down(KeyCode::Down){
 
-            first_paddle_pos. y += paddle_speed * delta_time;
-            second_paddle_pos.y += paddle_speed * delta_time;
+            paddle_pos. y += paddle_speed * delta_time;
         }
         
         ball_pos += ball_vel * delta_time;
 
-        if ball_pos.x > screen_width() - ball_size{
+        if ball_pos.x > screen_width() - ball_size - paddle_width{
             ball_vel.x = -ball_vel.x;
+            if ball_pos.y < paddle_pos.y - ball_size ||
+               ball_pos.y > paddle_pos.y + paddle_height {
+                quit();
+            }
         }
         
-        if ball_pos.y > screen_height() - ball_size{
+        if ball_pos.y > screen_height() - ball_size - paddle_width{
             ball_vel.y = -ball_vel.y;
         }
         
-        if ball_pos.x < 0.{
+        if ball_pos.x < paddle_width{
             ball_vel.x = -ball_vel.x;
+            if ball_pos.y < paddle_pos.y - ball_size ||
+               ball_pos.y > paddle_pos.y + paddle_height {
+                quit();
+            }
         }
 
         if ball_pos.y < 0.{
@@ -49,8 +55,8 @@ async fn main() {
         draw_text(get_fps().to_string().as_str(), 16., 32., 32., WHITE);
         
         draw_rectangle(ball_pos.x, ball_pos.y, ball_size, ball_size, WHITE);
-        draw_rectangle(first_paddle_pos.x, first_paddle_pos.y, paddle_width, paddle_height, WHITE);
-        draw_rectangle(second_paddle_pos.x, second_paddle_pos.y, paddle_width, paddle_height, WHITE);
+        draw_rectangle(0.,                            paddle_pos.y, paddle_width, paddle_height, WHITE);
+        draw_rectangle(screen_width() - paddle_width, paddle_pos.y, paddle_width, paddle_height, WHITE);
 
         next_frame().await
     }
