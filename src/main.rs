3deac1f6f4e1af0b4cc8452ba92f848e768c94ed
Wiktor Janecki <j.wiktor05@interia.pl>
2024-12-04 20:03:14 +0100

    Refactor variables to vectors

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,34 @@ use macroquad::prelude::*;
 #[macroquad::main("Tytuł Aplikacji")]
 async fn main() {
     let ball_size = 50.;
+    let mut ball_pos = Vec2::new(0.,0.);
+    let mut ball_vel = Vec2::new(200.,200.);
 
-    let mut pos_x = 200.;
-    let mut pos_y = 200.;
-
-    let mut vel_x = 200.;
-    let mut vel_y = 200.;
     loop{
         let delta_time = get_frame_time();
-        pos_x += delta_time * vel_x;
-        pos_y += delta_time * vel_y;
+        ball_pos += ball_vel * delta_time;
 
-        if pos_x > screen_width() - ball_size{
-            vel_x = -vel_x;
+        if ball_pos.x > screen_width() - ball_size{
+            ball_vel.x = -ball_vel.x;
         }
         
-        if pos_y > screen_height() - ball_size{
-            vel_y = -vel_y;
+        if ball_pos.y > screen_height() - ball_size{
+            ball_vel.y = -ball_vel.y;
         }
         
-        if pos_x < 0.{
-            vel_x = -vel_x;
+        if ball_pos.x < 0.{
+            ball_vel.x = -ball_vel.x;
         }
 
-        if pos_y < 0.{
-            vel_y = -vel_y;
+        if ball_pos.y < 0.{
+            ball_vel.y = -ball_vel.y;
         }
 
 
         // before this line show drawing string, error message with just getfps and with tostring
         draw_text(get_fps().to_string().as_str(), 16., 32., 32., WHITE);
         
-        draw_rectangle(pos_x, pos_y, ball_size, ball_size, WHITE);
+        draw_rectangle(ball_pos.x, ball_pos.y, ball_size, ball_size, WHITE);
 
         next_frame().await
     }
