a24d6c196157ace33c341e7ad462a5331c07642f
Wiktor Janecki <j.wiktor05@interia.pl>
2024-12-04 19:50:06 +0100

    Add velocity

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Tytuł Aplikacji")]
 async fn main() {
-    let pos_x = 200.;
-    let pos_y = 200.;
+    let mut pos_x = 200.;
+    let mut pos_y = 200.;
+
+    let vel_x = 50.;
+    let vel_y = 50.;
     loop{
+        let delta_time = get_frame_time();
+        pos_x += delta_time * vel_x;
+        pos_y += delta_time * vel_y;
+        
+
+
         // before this line show drawing string, error message with just getfps and with tostring
         draw_text(get_fps().to_string().as_str(), 16., 32., 32., WHITE);
         
