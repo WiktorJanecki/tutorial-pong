de6f9bdd1200c190588ded65f7838cd78b4e3c57
Wiktor Janecki <j.wiktor05@interia.pl>
2024-12-04 19:47:19 +0100

    Add player

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Tytuł Aplikacji")]
 async fn main() {
+    let pos_x = 200.;
+    let pos_y = 200.;
     loop{
+        // before this line show drawing string, error message with just getfps and with tostring
+        draw_text(get_fps().to_string().as_str(), 16., 32., 32., WHITE);
+        
+        draw_rectangle(pos_x, pos_y, 50., 50., WHITE);
+
         next_frame().await
     }
 }
