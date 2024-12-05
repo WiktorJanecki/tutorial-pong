062656bee67f8a2e29b2c9062ef2b0fbbe78701f
Wiktor Janecki <j.wiktor05@interia.pl>
2024-12-05 21:54:58 +0100

    Add sounds

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use macroquad::prelude::*;
+use macroquad::{audio::{load_sound, play_sound_once}, prelude::*};
 
 #[macroquad::main("Tytuł Aplikacji")]
 async fn main() {
+    // TODO: ADD GAMEOVER SOUND AND POINT SOUND
     let paddle_txt= load_texture("assets/paddle.png").await.unwrap();
     let ball_txt= load_texture("assets/ball.png").await.unwrap();
-    let font = load_ttf_font("./assets/SQUARED2.ttf").await.unwrap();
+    let font = load_ttf_font("assets/SQUARED2.ttf").await.unwrap();
+    let ball_sound = load_sound("assets/ping.wav").await.unwrap();
+    let gameover_sound = load_sound("assets/gameover.wav").await.unwrap();
 
     let ball_size = 30.;
     let mut ball_pos = Vec2::new(100.,100.);
@@ -32,24 +35,32 @@ async fn main() {
         ball_pos += ball_vel * delta_time;
 
         if ball_pos.x > screen_width() - ball_size - paddle_width{
-            ball_vel.x = -ball_vel.x;
-            score+=1;
             if ball_pos.y < paddle_pos.y - ball_size || ball_pos.y > paddle_pos.y + paddle_height {
                 ball_pos = Vec2::new(100., 100.);
                 score = 0;
+                play_sound_once(&gameover_sound);
+            }
+            else{
+                ball_vel.x = -ball_vel.x;
+                play_sound_once(&ball_sound);
+                score+=1;
             }
         }
         
-        if ball_pos.y > screen_height() - ball_size - paddle_width{
+        if ball_pos.y > screen_height() - ball_size{
             ball_vel.y = -ball_vel.y;
         }
         
         if ball_pos.x < paddle_width{
-            ball_vel.x = -ball_vel.x;
-            score+=1;
             if ball_pos.y < paddle_pos.y - ball_size || ball_pos.y > paddle_pos.y + paddle_height {
                 ball_pos = Vec2::new(100., 100.);
                 score = 0;
+                play_sound_once(&gameover_sound);
+            }
+            else{
+                ball_vel.x = -ball_vel.x;
+                score+=1;
+                play_sound_once(&ball_sound);
             }
         }
 
